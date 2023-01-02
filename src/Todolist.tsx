74f619e73b4f102b6492c24f0ef77d55a137be5b
yegorgursky <yahorhurski@gmail.com>
2023-01-02 19:29:40 +0300

    added filter buttons

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -18,29 +18,32 @@ export const Todolist: React.FC<PropsType> = (props) => {
                 <h3>{props.title}</h3>
                 <input/>
             </div>
-            <div>
-                <div style={{listStyle: 'none', paddingTop: '15px'}}>
-                    <li>
-                        <input type="checkbox" checked={props.tasks[0].isDone}/>
-                        {props.tasks[0].title}
-                    </li>
-                    <li>
-                        <input type="checkbox" checked={props.tasks[1].isDone}/>
-                        {props.tasks[1].title}
-                    </li>
-                    <li>
-                        <input type="checkbox" checked={props.tasks[2].isDone}/>
-                        {props.tasks[2].title}
-                    </li>
-                    <li>
-                        <input type="checkbox" checked={props.tasks[3].isDone}/>
-                        {props.tasks[3].title}
-                    </li>
-                    <li>
-                        <input type="checkbox" checked={props.tasks[4].isDone}/>
-                        {props.tasks[4].title}
-                    </li>
-                </div>
+            <div style={{listStyle: 'none', paddingTop: '15px'}}>
+                <li>
+                    <input type="checkbox" checked={props.tasks[0].isDone}/>
+                    {props.tasks[0].title}
+                </li>
+                <li>
+                    <input type="checkbox" checked={props.tasks[1].isDone}/>
+                    {props.tasks[1].title}
+                </li>
+                <li>
+                    <input type="checkbox" checked={props.tasks[2].isDone}/>
+                    {props.tasks[2].title}
+                </li>
+                <li>
+                    <input type="checkbox" checked={props.tasks[3].isDone}/>
+                    {props.tasks[3].title}
+                </li>
+                <li>
+                    <input type="checkbox" checked={props.tasks[4].isDone}/>
+                    {props.tasks[4].title}
+                </li>
+            </div>
+            <div style={{paddingTop: '15px'}}>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
             </div>
         </div>
     );
