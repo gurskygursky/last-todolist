3a067ed58c6ce7f8cc084abd93a1095e26f63877
yegorgursky <yahorhurski@gmail.com>
2023-01-02 19:40:55 +0300

    use map for tasks

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -19,26 +19,30 @@ export const Todolist: React.FC<PropsType> = (props) => {
                 <input/>
             </div>
             <div style={{listStyle: 'none', paddingTop: '15px'}}>
-                <li>
-                    <input type="checkbox" checked={props.tasks[0].isDone}/>
-                    {props.tasks[0].title}
-                </li>
-                <li>
-                    <input type="checkbox" checked={props.tasks[1].isDone}/>
-                    {props.tasks[1].title}
-                </li>
-                <li>
-                    <input type="checkbox" checked={props.tasks[2].isDone}/>
-                    {props.tasks[2].title}
-                </li>
-                <li>
-                    <input type="checkbox" checked={props.tasks[3].isDone}/>
-                    {props.tasks[3].title}
-                </li>
-                <li>
-                    <input type="checkbox" checked={props.tasks[4].isDone}/>
-                    {props.tasks[4].title}
-                </li>
+                {props.tasks.map((task: TaskType) => <li>
+                    <input type={'checkbox'} checked={task.isDone}/>
+                    {task.title}
+                </li>)}
+                {/*<li>*/}
+                {/*    <input type="checkbox" checked={props.tasks[0].isDone}/>*/}
+                {/*    {props.tasks[0].title}*/}
+                {/*</li>*/}
+                {/*<li>*/}
+                {/*    <input type="checkbox" checked={props.tasks[1].isDone}/>*/}
+                {/*    {props.tasks[1].title}*/}
+                {/*</li>*/}
+                {/*<li>*/}
+                {/*    <input type="checkbox" checked={props.tasks[2].isDone}/>*/}
+                {/*    {props.tasks[2].title}*/}
+                {/*</li>*/}
+                {/*<li>*/}
+                {/*    <input type="checkbox" checked={props.tasks[3].isDone}/>*/}
+                {/*    {props.tasks[3].title}*/}
+                {/*</li>*/}
+                {/*<li>*/}
+                {/*    <input type="checkbox" checked={props.tasks[4].isDone}/>*/}
+                {/*    {props.tasks[4].title}*/}
+                {/*</li>*/}
             </div>
             <div style={{paddingTop: '15px'}}>
                 <button>All</button>
