aec467b491548c76b0c77fba5a42b4af1966c97c
yegorgursky <yahorhurski@gmail.com>
2023-01-02 18:27:08 +0300

    create todolists

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 export function App() {
   return (
     <div className="App">
-      TODOLIST
+      <Todolist />
+      <Todolist />
+      <Todolist />
+
     </div>
   );
 }
+
+export const Todolist: React.FC = (props) => {
+  return (
+    <div>
+      <div>
+      <h3>What to learn</h3>
+      <input />
+      </div>
+      <div>
+        <div style={{listStyle: 'none', paddingTop: '15px'}}>
+          <li>
+          <input type='checkbox' checked={true}/>
+            HTML & CSS
+          </li>
+          <li>
+          <input type='checkbox' checked={true}/>
+            Javascript
+          </li>
+          <li>
+          <input type='checkbox' checked={false}/>
+            React
+          </li>
+        </div>
+      </div>
+    </div>
+  );
+};
