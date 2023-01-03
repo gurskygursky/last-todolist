5e47f510b0e6a4ef2e73981cd949e4e783cd53a0
yegorgursky <yahorhurski@gmail.com>
2023-01-03 22:17:31 +0300

    typing tasks filter, fixed key in map, refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
 import React, {useState} from 'react';
 import './App.css';
-import {TaskType, Todolist} from './Todolist';
+import {TasksFilterType, TaskType, Todolist} from './Todolist';
 
 export const App: React.FC = () => {
 
-    // const tasks1 = [
-    //     {id: 1, title: 'HTML&CSS', isDone: true},
-    //     {id: 2, title: 'JS', isDone: true},
-    //     {id: 3, title: 'ReactJS', isDone: false},
-    //     {id: 4, title: 'NodeJS', isDone: false},
-    //     {id: 5, title: 'Swift', isDone: false},
-    // ];
-    // const tasks2 = [
-    //     {id: 1, title: 'The Madness Of Crowds', isDone: true},
-    //     {id: 2, title: 'Atomic Habits:', isDone: true},
-    //     {id: 3, title: 'The Rise and Fall of the Third Reich', isDone: true},
-    //     {id: 4, title: 'JavaScript for Kids', isDone: true},
-    //     {id: 5, title: `JavaScript Absolute Beginner's Guide`, isDone: false},
-    // ];
-
     const [tasks, setTasks] = useState([
-            {id: 1, title: 'The Madness Of Crowds', isDone: true},
-            {id: 2, title: 'Atomic Habits:', isDone: true},
-            {id: 3, title: 'The Rise and Fall of the Third Reich', isDone: true},
-            {id: 4, title: 'JavaScript for Kids', isDone: true},
-            {id: 5, title: `JavaScript Absolute Beginner's Guide`, isDone: false},
+        {id: 1, title: 'The Madness Of Crowds', isDone: true},
+        {id: 2, title: 'Atomic Habits:', isDone: true},
+        {id: 3, title: 'The Rise and Fall of the Third Reich', isDone: true},
+        {id: 4, title: 'JavaScript for Kids', isDone: true},
+        {id: 5, title: `JavaScript Absolute Beginner's Guide`, isDone: false},
     ]);
 
-    const [filter, setFilter] = useState<'All' | 'Active' | 'Completed'>('All');
+    const [filter, setFilter] = useState<TasksFilterType>('All');
 
     const removeTask = (taskID: number) => {
         setTasks(tasks.filter((task: TaskType) => task.id !== taskID));
@@ -44,9 +29,11 @@ export const App: React.FC = () => {
 
     return (
         <div className="App">
-            {/*<Todolist title={'What to learn'} tasks={tasks1}/>*/}
-            {/*<Todolist title={'What to buy'} tasks={tasks2}/>*/}
-            <Todolist title={'Reading list'} tasks={tasksForTodolist} removeTask={removeTask} setFilter={setFilter}/>
+            <Todolist title={'Reading list'}
+                      tasks={tasksForTodolist}
+                      removeTask={removeTask}
+                      setFilter={setFilter}
+            />
         </div>
     );
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -6,11 +6,13 @@ export type TaskType = {
     isDone: boolean;
 }
 
+export type TasksFilterType = 'All' | 'Active' | 'Completed';
+
 type PropsType = {
     title: string;
     tasks: Array<TaskType>;
     removeTask: (taskID: number) => void;
-    setFilter: (filter: 'All' | 'Active' | 'Completed') => void;
+    setFilter: (filter: TasksFilterType) => void;
 }
 
 export const Todolist: React.FC<PropsType> = (props) => {
@@ -36,31 +38,11 @@ export const Todolist: React.FC<PropsType> = (props) => {
                 <input/>
             </div>
             <div style={{listStyle: 'none', paddingTop: '15px'}}>
-                {props.tasks.map((task: TaskType) => <li>
+                {props.tasks.map((task: TaskType) => <li key={task.id}>
                     <input type={'checkbox'} checked={task.isDone}/>
                     {task.title}
                     <button onClick={() => removeTask(task.id)}>x</button>
                 </li>)}
-                {/*<li>*/}
-                {/*    <input type="checkbox" checked={props.tasks[0].isDone}/>*/}
-                {/*    {props.tasks[0].title}*/}
-                {/*</li>*/}
-                {/*<li>*/}
-                {/*    <input type="checkbox" checked={props.tasks[1].isDone}/>*/}
-                {/*    {props.tasks[1].title}*/}
-                {/*</li>*/}
-                {/*<li>*/}
-                {/*    <input type="checkbox" checked={props.tasks[2].isDone}/>*/}
-                {/*    {props.tasks[2].title}*/}
-                {/*</li>*/}
-                {/*<li>*/}
-                {/*    <input type="checkbox" checked={props.tasks[3].isDone}/>*/}
-                {/*    {props.tasks[3].title}*/}
-                {/*</li>*/}
-                {/*<li>*/}
-                {/*    <input type="checkbox" checked={props.tasks[4].isDone}/>*/}
-                {/*    {props.tasks[4].title}*/}
-                {/*</li>*/}
             </div>
             <div style={{paddingTop: '15px'}}>
                 <button onClick={allTasks}>All</button>
