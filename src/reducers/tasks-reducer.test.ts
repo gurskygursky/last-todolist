848a9ac49e285253b2ccfe92b6a28a8d915533f7
yahorhurski <yahorhurski@gmail.com>
2023-02-03 16:15:03 +0300

    test case adding task

--- src/reducers/tasks-reducer.test.ts
+++ src/reducers/tasks-reducer.test.ts
@@ -1,6 +1,6 @@
 import {v1} from 'uuid';
-import {TasksType} from './../Todolist';
-import {removeTaskAC, tasksReducer} from './../reducers/tasks-reducer';
+import { TasksType } from '../Todolist';
+import {addTaskAC, removeTaskAC, tasksReducer} from './tasks-reducer';
 
 test('task should be removed is correct', () => {
 
@@ -45,4 +45,37 @@ test('task should be removed is correct', () => {
             {id: '5', title: 'Node.JS', isDone: false},
         ]
     );
+});
+
+test('correct task should be added to correct array', () => {
+
+    const todolistID1 = v1();
+    const todolistID2 = v1();
+
+    const startState: TasksType = {
+        [todolistID1]: [
+            {id: '1', title: 'The Madness Of Crowds', isDone: true},
+            {id: '2', title: 'Atomic Habits:', isDone: true},
+            {id: '3', title: 'The Rise and Fall of the Third Reich', isDone: true},
+            {id: '4', title: 'JavaScript for Kids', isDone: true},
+            {id: '5', title: `JavaScript Absolute Beginner's Guide`, isDone: false},
+        ],
+        [todolistID2]: [
+            {id: '1', title: 'HTML & CSS', isDone: true},
+            {id: '2', title: 'Javascript', isDone: true},
+            {id: '3', title: 'Typescript', isDone: true},
+            {id: '4', title: 'React', isDone: true},
+            {id: '5', title: 'Node.JS', isDone: false},
+        ],
+    };
+
+    const action = addTaskAC(todolistID2, 'Nest JS');
+
+    const endState = tasksReducer(startState, action);
+
+    expect(endState[todolistID1].length).toBe(5);
+    expect(endState[todolistID2].length).toBe(6);
+    expect(endState[todolistID2][0].id).toBeDefined();
+    expect(endState[todolistID2][0].title).toBe('Nest JS');
+    expect(endState[todolistID2][0].isDone).toBe(false);
 });
--- src/reducers/tasks-reducer.ts
+++ src/reducers/tasks-reducer.ts
@@ -1,12 +1,14 @@
-import {TasksType, TaskType} from './../Todolist';
-import {RemoveTodolistActionType} from './../reducers/todolist-reducer';
+import {v1} from "uuid";
+import {TasksType, TaskType} from "../Todolist";
+import { RemoveTodolistActionType } from "./todolist-reducer";
 
 enum ACTIONS {
     REMOVE_TASK = 'REMOVE_TASK',
     REMOVE_TODOLIST = 'REMOVE_TODOLIST',
+    ADD_TASK = 'ADD_TASK',
 }
 
-export const tasksReducer = (state: TasksType, action: TasksReducerActionsType | RemoveTodolistActionType) => {
+export const tasksReducer = (state: TasksType, action: TasksReducerActionsType | RemoveTodolistActionType): TasksType => {
     switch (action.type) {
         case ACTIONS.REMOVE_TASK:
             return {
@@ -17,17 +19,35 @@ export const tasksReducer = (state: TasksType, action: TasksReducerActionsType |
             return {
                 ...state, [action.payload.todolistID]: []
             }
+        case ACTIONS.ADD_TASK:
+            return {
+                ...state,
+                [action.payload.todolistID]: [
+                    {
+                        id: v1(),
+                        title: action.payload.title,
+                        isDone: false
+                    }, ...state[action.payload.todolistID]
+                ]
+            }
         default:
             return state;
     }
 };
 
 type RemoveTaskActionType = ReturnType<typeof removeTaskAC>;
-type TasksReducerActionsType = RemoveTaskActionType;
+type AddTaskActionType = ReturnType<typeof addTaskAC>;
+type TasksReducerActionsType = RemoveTaskActionType | AddTaskActionType;
 
 export const removeTaskAC = (todolistID: string, taskID: string) => {
     return {
         type: ACTIONS.REMOVE_TASK,
         payload: {todolistID, taskID}
+    } as const
+}
+export const addTaskAC = (todolistID: string, title: string) => {
+    return {
+        type: ACTIONS.ADD_TASK,
+        payload: {todolistID, title}
     } as const
 }
