04a6cf221521220efed95f91264b3f8d0ba98018
yahorhurski <yahorhurski@gmail.com>
2023-02-04 21:08:26 +0300

    test case changing title todolist

--- src/reducers/todolist-reducer.test.ts
+++ src/reducers/todolist-reducer.test.ts
@@ -1,6 +1,6 @@
 import {v1} from 'uuid';
-import {removeTodolistAC, todolistReducer} from '../reducers/todolist-reducer';
-import { TodolistType } from '../Todolist';
+import {changeTodolistTitleAC, removeTodolistAC, todolistReducer} from '../reducers/todolist-reducer';
+import {TodolistType} from '../Todolist';
 
 test('task should be removed is correct', () => {
 
@@ -18,3 +18,22 @@ test('task should be removed is correct', () => {
     expect(endState.length).toBe(1);
     expect(endState[0].id).toBe(todolistID2);
 });
+
+test('correct todolist should change its name', () => {
+
+    let todolistID1 = v1();
+    let todolistID2 = v1();
+
+    const startState: Array<TodolistType> = [
+        {id: todolistID1, title: 'Reading list', filter: 'All'},
+        {id: todolistID2, title: 'What to learn ', filter: 'All'},
+    ]
+
+    const action = changeTodolistTitleAC(todolistID2, 'new todolist');
+
+    const endState = todolistReducer(startState, action);
+
+    expect(endState[0].title).toBe('Reading list')
+    expect(endState[1].title).toBe('new todolist')
+});
+
--- src/reducers/todolist-reducer.ts
+++ src/reducers/todolist-reducer.ts
@@ -1,9 +1,10 @@
 import {v1} from 'uuid';
-import { TodolistType } from '../Todolist';
+import {TodolistType} from '../Todolist';
 
 enum ACTIONS {
     REMOVE_TODOLIST = 'REMOVE_TODOLIST',
     ADD_TODOLIST = 'ADD_TODOLIST',
+    CHANGE_TODOLIST_TITLE = 'CHANGE_TODOLIST_TITLE',
 }
 
 export const todolistReducer = (state: Array<TodolistType>, action: TodolistReducerActionsType) => {
@@ -12,6 +13,10 @@ export const todolistReducer = (state: Array<TodolistType>, action: TodolistRedu
             return state.filter((todolist: TodolistType) => todolist.id !== action.payload.todolistID);
         case ACTIONS.ADD_TODOLIST:
             return [{id: action.payload.todolistID, title: action.payload.title, filter: "All"}, ...state];
+        case ACTIONS.CHANGE_TODOLIST_TITLE:
+            return state.map((todolist: TodolistType) => todolist.id === action.payload.todolistID
+                ? {...todolist, title: action.payload.title}
+                : todolist)
         default:
             return state;
     }
@@ -19,7 +24,8 @@ export const todolistReducer = (state: Array<TodolistType>, action: TodolistRedu
 
 export type RemoveTodolistActionType = ReturnType<typeof removeTodolistAC>;
 export type AddTodolistActionType = ReturnType<typeof addTodolistAC>;
-type TodolistReducerActionsType = RemoveTodolistActionType | AddTodolistActionType;
+export type ChangeTodolistTitleActionType = ReturnType<typeof changeTodolistTitleAC>;
+type TodolistReducerActionsType = RemoveTodolistActionType | AddTodolistActionType | ChangeTodolistTitleActionType;
 
 export const removeTodolistAC = (todolistID: string) => {
     return {
@@ -31,5 +37,12 @@ export const addTodolistAC = (title: string) => {
     return {
         type: ACTIONS.ADD_TODOLIST,
         payload: {todolistID: v1(), title}
+    } as const
+}
+
+export const changeTodolistTitleAC = (todolistID: string, title: string) => {
+    return {
+        type: ACTIONS.CHANGE_TODOLIST_TITLE,
+        payload: {todolistID, title}
     } as const
 }
