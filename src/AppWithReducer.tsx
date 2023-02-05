f47173060c2c4c38f7f6c0017254c78a11d30d7a
yahorhurski <yahorhurski@gmail.com>
2023-02-05 21:41:32 +0300

    refactoring app with reducer component

--- src/AppWithReducer.tsx
+++ src/AppWithReducer.tsx
@@ -41,60 +41,37 @@ export const AppWithReducer: React.FC = () => {
 
     const removeTask = (todolistID: string, taskID: string) => {
         setTasksDispatch(removeTaskAC(todolistID, taskID));
-        // setTasks({...tasks, [todolistID]: tasks[todolistID].filter((task: TaskType) => task.id !== taskID)});
     }
 
     const addTask = (todolistID: string, title: string) => {
         setTasksDispatch(addTaskAC(todolistID, title));
-        // setTasks({...tasks, [todolistID]: [{id: v1(), title, isDone: false}, ...tasks[todolistID]]});
     }
 
     const changeTasksFilter = (todolistID: string, filter: TasksFilterType) => {
         setListsDispatch(changeTodolistFilterAC(todolistID, filter));
-        // setLists(lists.map(list => list.id === todolistID ? {...list, filter} : list));
     }
 
     const changeTaskStatus = (todolistID: string, taskID: string, isDone: boolean) => {
         setTasksDispatch(changeTaskStatusAC(todolistID, taskID, isDone));
-        // setTasks({
-        //     ...tasks, [todolistID]: tasks[todolistID].map((task: TaskType) => task.id === taskID
-        //         ? {...task, isDone}
-        //         : task)
-        // })
     }
 
     const removeTodolist = (todolistID: string) => {
         const action = removeTodolistAC(todolistID);
         setListsDispatch(action);
         setTasksDispatch(action);
-        // setLists(lists.filter((list: TodolistType) => list.id !== todolistID));
-        // delete tasks[todolistID];
-        // setTasks({...tasks});
-        // console.log(tasks);
     }
 
     const addTodolist = (value: string) => {
         const action = addTodolistAC(value);
         setListsDispatch(action);
         setTasksDispatch(action);
-        // const newTodolistID = v1();
-        // const newTodolist: TodolistType = {id: newTodolistID, title: value, filter: 'All'};
-        // setLists([...lists, newTodolist]);
-        // setTasks({[newTodolistID]: [], ...tasks});
     }
 
     const changeTodolistTitle = (todolistID: string, value: string) => {
         setListsDispatch(changeTodolistTitleAC(todolistID, value));
-        // setLists(lists.map((list: TodolistType) => list.id === todolistID ? {...list, title: value} : list));
     }
     const changeTaskTitle = (todolistID: string, taskID: string, value: string) => {
         setTasksDispatch(changeTaskTitleAC(todolistID, taskID, value));
-        // setTasks({
-        //     ...tasks, [todolistID]:
-        //         tasks[todolistID].map((task: TaskType) => task.id === taskID
-        //             ? {...task, title: value}
-        //             : task)
-        // });
     }
 
     return (
